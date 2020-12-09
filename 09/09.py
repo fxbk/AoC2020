c30cf32c0d0645dcaf3c2ef223f4f64cec69b015
fxbk <44337498+fxbk@users.noreply.github.com>
2020-12-09 08:35:25 +0100

    Solution day 9 part 2

--- 09/09.py
+++ 09/09.py
@@ -1,7 +1,6 @@
 file = open('input.txt', 'r')
 input = file.read().split('\n')
 input = [int(x) for x in input]
-print(input)
 
 
 def get_possible_sum(data, preamble_length, idx):
@@ -19,7 +18,20 @@ def check_input(data, preamble_length):
     for idx, x in enumerate(data[preamble_length+1:]):
         idx += preamble_length + 1
         if x not in get_possible_sum(data, preamble_length, idx):
-            print(x)
+            return x
             break
 
-check_input(input, 25)
+invalid_numer = check_input(input, 25)
+print(f'Solution part 1: {invalid_numer}')
+
+for idx, x in enumerate(input):
+    summands = []
+    sum = 0
+    i = 0
+    while sum < invalid_numer:
+        sum += input[idx + i]
+        summands.append(input[idx + i])
+        i += 1
+    if sum == invalid_numer:
+        print(f'Solution part 2: {min(summands) + max(summands)}')
+        break
