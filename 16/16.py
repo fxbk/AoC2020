c19d6aac3d87765cd0e8047c6f4e14f80f7a0ec6
fxbk <44337498+fxbk@users.noreply.github.com>
2020-12-16 19:33:20 +0100

    Trying out new approach

--- 16/16.py
+++ 16/16.py
@@ -1,4 +1,4 @@
-file = open('input.txt', 'r')
+file = open('input2.txt', 'r')
 input = file.read()
 input = input.split('\n\n')
 
@@ -6,14 +6,11 @@ fields_values = input[0].split('\n')
 fields = [x.split(': ')[0] for x in fields_values]
 ranges = [(x[0].split('-'), x[1].split('-')) for x in [x.split(': ')[1].split(' or ') for x in fields_values]]
 fields_values_dict = dict(zip(fields, ranges))
-print(fields_values_dict)
 
 my_ticket = input[1].split('\n')[1].split(',')
-print(my_ticket)
 
 nearby_tickets = input[2].split('\n')
 nearby_tickets = [x.split(',') for x in nearby_tickets[1:]]
-print(nearby_tickets)
 
 
 def check_value(value, fields_values_dict):
@@ -34,4 +31,40 @@ for ticket in nearby_tickets:
         if not any(check_value(value, fields_values_dict)):
             error += value
 
-print(error)
+print(f'Solution part 1: {error}')
+
+# Part 2
+cleaned_tickets = []
+for ticket in nearby_tickets:
+    valid = True
+    for value in ticket:
+        value = int(value)
+        if not any(check_value(value, fields_values_dict)):
+            valid = False
+    if valid:
+        cleaned_tickets.append(ticket)
+import numpy as np
+fields_in_right_order = []
+
+
+def combine_checks(list_of_lists):
+    out = []
+    for list in list_of_lists:
+        tmp = []
+        for idx in np.arange(len(list))[::2]:
+            tmp.append(list[idx] or list[idx+1])
+        out.append(tmp)
+    return out
+
+
+for field, ranges in fields_values_dict.items():
+    valid_list = []
+    for ticket in cleaned_tickets:
+        for value in ticket:
+            value = int(value)
+            valid_list.append(check_value(value, fields_values_dict))
+    valid_list = combine_checks(valid_list)
+    valid_matrix = np.matrix(valid_list)
+    fields_in_right_order.append(fields[np.argmax(np.sum(valid_matrix, axis=0))])
+
+print(fields_in_right_order)
