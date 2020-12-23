cb9bf1b96e1c8c0a431e3cbce47e4ec3c55c94a8
fxbk <44337498+fxbk@users.noreply.github.com>
2020-12-23 20:42:10 +0100

    Implemented first solution to part 2

--- 18/18.py
+++ 18/18.py
@@ -1,7 +1,8 @@
-file = open('input.txt', 'r')
+file = open('input2.txt', 'r')
 input = file.read()
 input = input.split('\n')
 print(input)
+
 
 def calculate_values(calculation):
     if calculation[0] == '(':
@@ -37,10 +38,51 @@ def calculate_values(calculation):
     return value, idx
 
 
-sum = 0
+result = 0
 for calculation in input:
     new_value, _ = calculate_values(calculation)
     print(new_value)
-    sum += new_value
+    result += new_value
 
-print(f'Solution part 1: {sum}')
+print(f'Solution part 1: {result}')
+
+# Part 2
+
+def calculate_values_advanced(calculation):
+    total_additions = sum([1 for c in calculation if c == '+'])
+    executed_additions = 0
+    total_multiplications = sum([1 for c in calculation if c == '*'])
+    executed_multiplications = 0
+    idx = 0
+    result = 1
+    calculation = list(calculation)
+    while executed_additions < total_additions:
+        if calculation[idx] == '+':
+            calculation[idx] = int(calculation[idx-2]) + int(calculation[idx+2])
+            del calculation[idx + 1]
+            del calculation[idx + 1]
+            del calculation[idx-1]
+            del calculation[idx-2]
+            executed_additions += 1
+            idx -= 3
+        idx += 1
+    idx = 0
+    while executed_multiplications < total_multiplications:
+        if calculation[idx] == '*':
+            calculation[idx] = int(calculation[idx - 2]) * int(calculation[idx + 2])
+            del calculation[idx + 1]
+            del calculation[idx + 1]
+            del calculation[idx - 1]
+            del calculation[idx - 2]
+            executed_multiplications += 1
+            idx -= 3
+        idx += 1
+    return calculation[0]
+
+result = 0
+for calculation in input:
+    new_value = calculate_values_advanced(calculation)
+    print(new_value)
+    result += new_value
+
+print(f'Solution part 2: {result}')
